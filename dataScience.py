93f59ca55092d6c3f6cda969b7b67183d8c29f9a
onlinegithubtut <45347220+onlinegithubtut@users.noreply.github.com>
2018-11-27 20:59:26 -0800

    List of important libraries for data science

---
+++ dataScience.py
@@ -0,0 +1,5 @@
+print("Python is very good for data analysis")
+print("Important data analysis libraries in python are:")
+List = ['Pandas', 'Numpy', 'Matplotlib']
+for item in List:
+    print(item)
