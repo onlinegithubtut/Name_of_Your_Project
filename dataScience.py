9fb84cc9e58b5dd8c5a05b98b9d65b362ad9bc50
onlinegithubtut <45347220+onlinegithubtut@users.noreply.github.com>
2018-11-27 21:02:14 -0800

    Changed list to dictionary

--- dataScience.py
+++ dataScience.py
@@ -1,5 +1,6 @@
 print("Python is very good for data analysis")
 print("Important data analysis libraries in python are:")
-List = ['Pandas', 'Numpy', 'Matplotlib']
-for item in List:
-    print(item)
+#Enumerate allows us to loop over something and have an automatic counter.
+Dict = {1:'Pandas', 2:'Numpy', 3:'Matplotlib'}
+for i, item in enumerate(Dict):
+    print("{}. {}".format(i, Dict[item]))
